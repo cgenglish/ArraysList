f199fb4cd4b69c70e9b92327bed7fca3683396d2
cgenglish <connor.g.english@gmail.com>
2024-04-15 08:17:57 -0700

    fixed issue with node.java and mylinkedlist.java

--- Program.java
+++ Program.java
@@ -8,13 +8,24 @@ public class Program {
 
     public static void main(String[] args) {
         MyLinkedList<String> myLinkedList = new MyLinkedList<String>();
-        myLinkedList.AddFront("World!");
+        myLinkedList.Append("World!");
         // Add a new Node here
-        myLinkedList.AddFront("Hello");
-        myLinkedList.AddFront("This is me");
-        myLinkedList.AddFront("Hi");
+        myLinkedList.Append("Hello");
+        myLinkedList.Append("This");
+        myLinkedList.Append("Hi");
         myLinkedList.Append("Dog");
+        myLinkedList.Insert(1, "Leaf");
+        myLinkedList.DeleteFront();
         System.out.println(myLinkedList.GetSize());
+        System.out.println(myLinkedList);
+        myLinkedList.Append("Cat");
+        System.out.println(myLinkedList);
+        myLinkedList.AddFront("Gato");
+        System.out.println(myLinkedList);
+        myLinkedList.DeleteBack();
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.GetSize());
+        myLinkedList.DeleteNodeBasedOnValue("Hello");
         System.out.println(myLinkedList);
     }
     
